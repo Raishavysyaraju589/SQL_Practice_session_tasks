dc76ec011bb034a1e56a76e24f30e3efcf87a738
raishavysyaraju@589 <raisha589@gmail.com>
2023-07-17 17:44:31 +0530

    updated the sql file

--- SQL_first.sql
+++ SQL_first.sql
@@ -1,1 +1,6 @@
-select * from Students;
+/*Assignments_Day1*/
+
+create database tasks;
+use tasks;
+create table todo_list(task_id int primary key,task_name varchar(100), description varchar(150), is_completed varchar(10));
+
