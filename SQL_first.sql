717911efcc5d6a1b49638846d548c5bec3b85393
raishavysyaraju@589 <raisha589@gmail.com>
2023-07-18 17:58:04 +0530

    SQL.second.sql

--- SQL_first.sql
+++ SQL_first.sql
@@ -2,5 +2,6 @@
 
 create database tasks;
 use tasks;
-create table todo_list(task_id int primary key,task_name varchar(100), description varchar(150), is_completed varchar(10));
+create table todo_list
+(task_id int primary key,task_name varchar(100), description varchar(150), is_completed varchar(10));
 
